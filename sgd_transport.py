27e5a4914a190b2cf1bf367699c2e823d00c4cd2
Dmitry Zhilenkov <zhildemon@gmail.com>
2019-07-23 23:54:12 +1000

    trigger for measures

--- sgd_transport.py
+++ sgd_transport.py
@@ -6,8 +6,9 @@ Created on Sun Jul 14 20:00:46 2019
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-def dualf(c, u, v, a, b, t=0.5, prevsg=0):
+def dualf(c, u, v, a, b, p, t=0.5, prevsg=0, trig_measure=True):
     matu = np.tile(u, (len(v), 1)).T
     matv = np.tile(v, (len(u), 1))
     r = matu + matv + c
@@ -18,10 +19,17 @@ def dualf(c, u, v, a, b, t=0.5, prevsg=0):
     j = np.sum(p * boolr, axis=0)
     veci = np.zeros(len(i))
     vecj = np.zeros(len(j))
-    idxi = np.random.choice(range(len(i)), p = i/np.sum(i))
-    idxj = np.random.choice(range(len(j)), p = j/np.sum(j))
-    veci[idxi] = i[idxi]
-    vecj[idxj] = j[idxj]
+    if trig_measure:
+        if (np.sum(i) != 0.) and (np.sum(j) != 0.):
+            idxi = np.random.choice(range(len(i)), p = i/np.sum(i))
+            idxj = np.random.choice(range(len(j)), p = j/np.sum(j))
+            veci[idxi] = i[idxi]
+            vecj[idxj] = j[idxj]
+    else:
+        idxi = np.random.choice(range(len(i)), p = np.array([1/len(i)]*len(i)))
+        idxj = np.random.choice(range(len(j)), p = np.array([1/len(j)]*len(j)))
+        veci[idxi] = i[idxi]
+        vecj[idxj] = j[idxj]
     wsg = np.concatenate((veci - a, vecj - b), axis=0)
     g = np.concatenate((-a + i, -b + j), axis=0)
     wsgu = veci - a
@@ -41,12 +49,12 @@ p = np.sum(a)
 # print(dualf(c, u, v, a, b, t=0.3, prevsg=0))
 
 mavwsg = np.concatenate((-a, -b), axis=0)
-alph = 1/100
+alph = 1e-4
 y = []
 
-for it in range(400):
-    res = dualf(c, u, v, a, b, t=0.7, prevsg=mavwsg)
-    u += alph/(it+1)*res[0]
+for it in range(10000):
+    res = dualf(c, u, v, a, b, p, t=0.7, prevsg=mavwsg, trig_measure=False)
+    u += alph*res[0]
     v += alph*res[1]
     wsg = res[2]
     g = res[3]
@@ -54,6 +62,7 @@ for it in range(400):
     obj = res[5]
     y.append(obj)
     
+print(y[-1])
 
-for i in y:
-    print(y)
+plt.figure()
+plt.plot(list(range(len(y))), y)
